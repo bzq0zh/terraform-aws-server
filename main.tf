e1580fc3e50879115787fefb0e21fb383e15614b
bzq0zh <32871424+bzq0zh@users.noreply.github.com>
2023-05-19 12:46:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2_example" {
 }
 
 resource "aws_security_group" "main" {
-    name        = "EC2-webserver-SG111anand-new"
+    name        = "EC2-webserver-SG111anand-new2"
   description = "Webserver for EC2 Instances"
 
   ingress {
